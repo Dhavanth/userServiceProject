e271bbfa0e8afcc18510f14786a79092bf617d3a
Dhavanth Mullangi <dhavanthm@gmail.com>
2024-02-24 16:37:20 +0530

    Added Envt Variables and deployed using AWS Ebs

--- src/test/java/org/example/userserviceproject/UserserviceprojectApplicationTests.java
+++ src/test/java/org/example/userserviceproject/UserserviceprojectApplicationTests.java
@@ -15,40 +15,40 @@ import java.util.UUID;
 
 @SpringBootTest
 class UserserviceprojectApplicationTests {
-
-    @Autowired
-    private JpaRegisteredClientRepository registeredClientRepository;
-
-    @Test
-    void contextLoads() {
-    }
-
-    // WHILE WRITING TO DB VIA TEST CASE, IT IS RECOMMENDED TO SE @Commit ANNOTATION TO SAVE THE DATA
-    // OTHERWISE IT GETS ROLLBACK
-
-    @Test
-    @Commit
-    void storeRegisteredClientIntoDB() {
-                RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("oidc-client")
-                .clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("https://oauth.pstmn.io/v1/callback")
-                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .scope("ADMIN")
-                .scope("STUDENT")
-                .scope("MENTOR") // Role
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-
-                registeredClientRepository.save(oidcClient);
-
-    }
-
-
-
+//
+//    @Autowired
+//    private JpaRegisteredClientRepository registeredClientRepository;
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    // WHILE WRITING TO DB VIA TEST CASE, IT IS RECOMMENDED TO SE @Commit ANNOTATION TO SAVE THE DATA
+//    // OTHERWISE IT GETS ROLLBACK
+//
+//    @Test
+//    @Commit
+//    void storeRegisteredClientIntoDB() {
+//                RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//                .clientId("oidc-client")
+//                .clientSecret("{noop}secret")
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .redirectUri("https://oauth.pstmn.io/v1/callback")
+//                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .scope("ADMIN")
+//                .scope("STUDENT")
+//                .scope("MENTOR") // Role
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//
+//                registeredClientRepository.save(oidcClient);
+//
+//    }
+//
+//
+//
 }
